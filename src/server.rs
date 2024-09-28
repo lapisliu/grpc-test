fe72181301f56e349e62ee6d3244a58f8b31d591
lapisliu <lxteng2016@126.com>
2024-09-28 14:49:31 -0400

    fix: server listen to all incoming ip.

--- src/server.rs
+++ src/server.rs
@@ -46,7 +46,7 @@ impl DataTransfer for MyDataTransfer {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    let addr = "0.0.0.0:50051".parse()?;
     let data_transfer = MyDataTransfer::default();
 
     println!("Server listening on {}", addr);
